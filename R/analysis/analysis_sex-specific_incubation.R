1021aeb6a3f69fe6d85d59c6f54a32315a263952
leberhartphillips <luke.eberhart@gmail.com>
2023-02-26 22:35:12 +0100

    perfected circular plot

--- R/analysis/analysis_sex-specific_incubation.R
+++ R/analysis/analysis_sex-specific_incubation.R
@@ -296,15 +296,21 @@ gettime(CM1858_dist_to_nest$sunrise[1], scale = "radian") * 180/pi
 gettime(CM1858_dist_to_nest$sunset[1], scale = "radian") * 180/pi
 # "#be9c2e""#016392"
 # test circlize figure
+{
 sectors = letters[1]
 circos.par("gap.degree" = 0, "cell.padding" = c(0, 0, 0, 0), "start.degree" = 90)
 circos.initialize(sectors, xlim = c(0, 60*60*24), )
-circos.trackPlotRegion(ylim = c(0, log(15000)), 
+circos.trackPlotRegion(ylim = c(0, log(20000)), 
                        track.height = 0.8, bg.border = NA, 
                        panel.fun = function(x, y) {
   draw.sector(start.degree = gettime(CM1858_dist_to_nest$sunrise[1], scale = "radian") * 180/pi + 57, 
               end.degree = gettime(CM1858_dist_to_nest$sunset[1], scale = "radian") * 180/pi + 57, 
-              col = alpha("grey70", 0.75), border = NA)
+              col = alpha("grey70", 0.75), border = NA, rou2 = 0.99, rou1 = 0.15)
+  draw.sector(0, 360, rou1 = 0.2, lwd = 1, lty = 1, border = alpha("grey70", 0.75))
+  draw.sector(0, 360, rou1 = 0.38, lwd = 1, lty = 1, border = alpha("grey70", 0.75))
+  draw.sector(0, 360, rou1 = 0.56, lwd = 1, lty = 1, border = alpha("grey70", 0.75))
+  draw.sector(0, 360, rou1 = 0.75,  lwd = 1, lty = 1, border = alpha("grey70", 0.75))
+  draw.sector(0, 360, rou1 = 0.935, lwd = 1, lty = 1, border = alpha("grey70", 0.75))                       
   F_d1 = c(filter(dist_to_nest_mod_predicts, sex == "F" & species == "SNPL") %>% pull(seconds_of_day),
          filter(dist_to_nest_mod_predicts, sex == "F" & species == "SNPL") %>% pull(seconds_of_day) %>% rev())
   F_d2 = c(filter(dist_to_nest_mod_predicts, sex == "F" & species == "SNPL") %>% pull(upper),
@@ -328,7 +334,7 @@ circos.trackPlotRegion(ylim = c(0, log(15000)),
                   mutate(dist_from_nest = log(dist_from_nest) + 0.0001) %>%
                            pull(dist_from_nest),
                 cex = 1,
-                col = alpha("#D95F02", 0.5),
+                col = alpha("#be9c2e", 0.85),
                 pch = 19)
   circos.points(filter(all_data, sex == "M" & species == "SNPL") %>%
                   pull(time_of_day) %>%
@@ -337,7 +343,7 @@ circos.trackPlotRegion(ylim = c(0, log(15000)),
                   mutate(dist_from_nest = log(dist_from_nest) + 0.0001) %>%
                   pull(dist_from_nest),
                 cex = 1,
-                col = alpha("#1B9E77", 0.5),
+                col = alpha("#016392", 0.85),
                 pch = 19)
   circos.xaxis(major.at = c(0, 60*60*1, 60*60*2, 60*60*3, 60*60*4, 60*60*5, 60*60*6,
                            60*60*7, 60*60*8, 60*60*9, 60*60*10, 60*60*11, 60*60*12,
@@ -347,11 +353,11 @@ circos.trackPlotRegion(ylim = c(0, log(15000)),
               labels = c("Midnight", as.character(c(1:11)), "Noon", as.character(c(13:23))),
               major.tick.length = 0.5, minor.ticks = 0)
   circos.yaxis(at = c(0, log(10), log(100), log(1000), log(10000)),
-               labels = c("0", "10", "100", "1000", "10000"))
-  
+               labels = c("0 m", "10 m", "100 m", "1000 m", "10000 m"))
 
 })
 circos.clear()
+}
 
 "#00FF0080"
 
